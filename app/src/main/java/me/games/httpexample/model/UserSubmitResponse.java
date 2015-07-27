d46c7d857c47b6ae69e158ef462dfe3512a6258e
Sudarshan <email@sudarshanz.com>
2015-07-27 08:33:10 +0545

    added HTTPCLENT'

---
+++ app/src/main/java/me/games/httpexample/model/UserSubmitResponse.java
@@ -0,0 +1,13 @@
+package me.games.httpexample.model;
+
+public class UserSubmitResponse {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- app/src/main/java/me/games/httpexample/network/ApiHelper.java
+++ app/src/main/java/me/games/httpexample/network/ApiHelper.java
@@ -9,20 +9,36 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.DefaultedHttpParams;
+import org.apache.http.params.HttpParams;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 import me.games.httpexample.model.Category;
 import me.games.httpexample.model.CategoryResponse;
+import me.games.httpexample.model.UserSubmitResponse;
 
 /**
  * Created by darshanz on 7/17/15.
@@ -31,6 +47,7 @@ public class ApiHelper {
 
     private Context context;
     private static final String CATEGORY_URL = "http://oshika.me/categories.json";
+    private static final String REGISTER_USER_URL = "htto://technoguff.getsandbox.com/users";
 
     public ApiHelper(Context context) {
         this.context = context;
@@ -54,13 +71,25 @@ public class ApiHelper {
         CategoryResponse categoryResponse = new CategoryResponse();
 
         try {
-            URL url = new URL(CATEGORY_URL);
+
+
+
+            HttpClient client = new DefaultHttpClient();
+
+            HttpGet getter = new HttpGet(CATEGORY_URL);
+
+            HttpResponse response = client.execute(getter);
+
+            HttpEntity entity = response.getEntity();
+
+            InputStream is = entity.getContent();
+            /*URL url = new URL(CATEGORY_URL);
 
             URLConnection connection = url.openConnection();
             connection.connect();
-            InputStream inputStream = connection.getInputStream();
+            InputStream inputStream = connection.getInputStream();*/
 
-             InputStreamReader reader = new InputStreamReader(inputStream, "utf-8");
+             InputStreamReader reader = new InputStreamReader(is, "utf-8");
              categoryResponse = new Gson().fromJson(reader, CategoryResponse.class);
 
 
@@ -72,5 +101,62 @@ public class ApiHelper {
         }
 
         return categoryResponse.getData();
+    }
+
+
+
+    public UserSubmitResponse submitUser(String firstName, String lastName,  String address, String country){
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(REGISTER_USER_URL);
+
+
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(0, new BasicNameValuePair("firstName", firstName));
+        params.add(1, new BasicNameValuePair("lastName", lastName));
+        params.add(2, new BasicNameValuePair("address", address));
+        params.add(3, new BasicNameValuePair("country", country));
+
+
+        UrlEncodedFormEntity entity = null;
+        try {
+             entity = new UrlEncodedFormEntity(params);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        post.setEntity(entity);
+
+        HttpResponse response = null;
+        try {
+           response  = client.execute(post);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpEntity responseEntity = response.getEntity();
+
+
+        InputStream is = null;
+        try {
+            is = responseEntity.getContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            /*URL url = new URL(CATEGORY_URL);
+
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();*/
+
+        InputStreamReader reader = null;
+        try {
+            reader = new InputStreamReader(is, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        UserSubmitResponse submitResponse = new Gson().fromJson(reader, UserSubmitResponse.class);
+
+
+        return submitResponse;
     }
 }
